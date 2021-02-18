a7d246002589793557cfdfc7c54138dfaf67abb5
nizamadizu7 <nizam.adizu@gmail.com>
2021-02-18 09:30:50 +0700

    tahap 2

---
+++ cara.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Cara</title>
+    <link rel="shortcut icon" href="images/polisi.ico">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- font Awesome CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Main Styles CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+        <div class="shadow">
+            <nav class="navbar navbar-fixed navbar-inverse form-shadow">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index">
+                            <img alt="Brand" src="images/lambangpolisi.png">
+                        </a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index">HOME</a></li>
+                            <li><a href="lapor">LAPOR</a></li>
+                            <li><a href="lihat">LIHAT PENGADUAN</a></li>
+                            <li class="active"><a href="">CARA</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+
+            <!-- content -->
+            <div class="main-content">
+
+                <h3>Cara Melapor</h3>
+        		<ol>
+        			<li><p>Klik menu &quot;Lapor&quot; untuk merekam pengaduan baru.</p></li>
+        			<li><p>Isi form &quot;Buat Laporan&quot; sesuai informasi yang Anda ketahui.</p></li>
+        			<li>
+                        <p>Perhatikan beberapa hal di bawah ini :
+                            <br>- Semua kotak yang ada wajib diisi.
+                            <br>- Pastikan informasi yang diberikan sesuai kenyataan.
+                        </p>
+                    </li>
+        			<li>
+                        <p>Setelah selesai mengisi, silakan klik tombol &quot;Kirim Pengaduan&quot;
+                        untuk melanjutkan proses pelaporan Anda.
+                        </p>
+                    </li>
+        			<li>
+                        <p>
+                            Catat dan Simpan dengan baik nomor pengaduan yang Anda peroleh saat membuat
+                            pengaduan untuk mengetahui status/tindak lanjut pengaduan yang Anda sampaikan.
+                        </p>
+                    </li>
+        			<li>
+                        <p>
+                            Polsek Ciseeng, Bogor akan menghubungi Anda melalui kontak yang telah Anda
+                            cantumkan dalam form laporan apabila laporan yang Anda sampaikan belum
+                            memenuhi kriteria untuk ditindaklanjuti.
+                        </p>
+                    </li>
+        		</ol>
+            <!-- end main-content -->
+            </div>
+
+            <hr>
+
+            <!-- Footer -->
+            <footer class="footer text-center">
+                <div class="row">
+                    <div class="col-md-4 mb-5 mb-lg-0">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item">
+                                <i class="fa fa-top fa-map-marker"></i>
+                            </li>
+                        </ul>
+                        <p class="mb-0">
+                            Jalan Raya Ciseeng No.1
+                            <br>Bogor, Jawa Barat
+                        </p>
+                        </div>
+                        <div class="col-md-4 mb-5 mb-lg-0">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item">
+                                    <i class="fa fa-top fa-rss"></i>
+                                </li>
+                                <li class="list-inline-item">
+                                    <h4 class="text-uppercase mb-4">Sosial Media</h4>
+                                </li>
+                            </ul>
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://web.facebook.com/DivHumasPolri/?rf=526377507463341&_rdc=1&_rdr">
+                                        <i class="fa fa-fw fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://twitter.com/DivHumas_Polri?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                                        <i class="fa fa-fw fa-twitter"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item">
+                                    <i class="fa fa-top fa-envelope-o"></i>
+                                </li>
+                                <li class="list-inline-item">
+                                    <h4 class="text-uppercase mb-4">Kontak</h4>
+                                </li>
+                            </ul>
+                            <p class="mb-0">
+                                0251-808707 <br>
+                                polsekciseeng@polri.go.id <br>
+                                polsekciseeng@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </footer>
+                <!-- /footer -->
+
+                <div class="copyright py-4 text-center text-white">
+                    <small>Copyright 2021 | Donny Irwansyah, Irfan Cahyanto, Nizam Aditya</small>
+                </div>
+                <!-- shadow -->
+        </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="js/bootstrap.js"></script>
+
+</body>
+</html>
